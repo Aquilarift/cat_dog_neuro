62b798e12a82d06848f12a923e29a3a63140f1a4
Aquilarift <kelatyt@gmail.com>
2022-04-17 18:52:08 +0300

    updated

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Conv2D, MaxPooling2D
-from tensorflow.python.keras.layers import Activation, Dropout, Flatten, Dense
+from statistics import mode
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, AveragePooling2D
+from tensorflow.keras.layers import Activation, Dropout, Flatten, Dense
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import BinaryCrossentropy
 
+import json
+import tensorflow as tf
+
+import matplotlib.pyplot as plt
 
 train_dir = 'train'
 
@@ -10,13 +18,13 @@ val_dir = 'val'
 
 test_dir = 'test'
 
-img_width, img_height = 256, 256
+img_width, img_height = 200, 200
 
 input_shape = (img_width, img_height, 3)
 
-epochs = 20
+epochs = 15
 
-batch_size = 8
+batch_size = 16
 
 nb_train_samples = 20000
 
@@ -26,28 +34,46 @@ nb_test_samples = 2500
 
 
 model = Sequential()
+
 model.add(Conv2D(32, (3, 3), input_shape=input_shape))
 model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Conv2D(32, (3, 3)))
-model.add(Activation('relu'))
+model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Conv2D(64, (3, 3)))
 model.add(Activation('relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(128, (3, 3)))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(256, (3, 3)))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Flatten())
-model.add(Dense(64))
+model.add(Dense(256))
 model.add(Activation('relu'))
+model.add(BatchNormalization())
 model.add(Dropout(0.5))
+
 model.add(Dense(1))
 model.add(Activation('sigmoid'))
 
 
-model.compile(loss='binary_crossentropy',
-              optimizer='adam',
+print(model.summary())
+
+model.compile(loss=BinaryCrossentropy(),
+              optimizer=Adam(learning_rate=0.001),
               metrics=['accuracy'])
 
 
@@ -74,13 +100,35 @@ test_generator = datagen.flow_from_directory(
 
 history = model.fit(
     train_generator,
-    batch_size=batch_size,
     epochs=epochs,
-    validation_data=val_generator,
-    validation_batch_size=batch_size)
+    validation_data=val_generator)
 
-scores = model.evaluate(test_generator, batch_size=batch_size)
+scores = model.evaluate(test_generator)
 
 print(scores[1])
 
-print(history.history.keys())
+
+def showAc():
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
+
+def showLoss():
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
+
+showAc()
+showLoss()
+
+model.save('modelfin.h5')
